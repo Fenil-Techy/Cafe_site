5fb11ab10c11c627e67312808f66ad56356bfc48
Fenil Kapopara <fenilkapopara34@gmail.com>
2026-01-06 15:09:30 +0530

    make navbar for mobile

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+import { CiMenuBurger } from "react-icons/ci";
 export const Header=()=>{
+
+    const[isOpen,setIsOpen]=useState(false)
+
     return(
-        <div>
-            <h1>Header</h1>
-        </div>
+        
+            <nav className="sticky top-0 z-50 flex justify-between items-center px-2 py-1 bg-[#0b727b] border">
+                <div className="pl-3">
+                    <img src="/navbar/logo.webp" alt="" className="w-12 h-full" />
+                </div>
+                <button onClick={()=>setIsOpen(!isOpen)} className="pr-3"><CiMenuBurger className="text-white text-2xl" /></button>
+                <div className={
+                    `${isOpen?"translate-x-0":"translate-x-full"}
+                    fixed top-0 right-0 w-64 h-full bg-[#0b727b] shadow-2xl transform transition-transform duration-300 z-50
+                    `}>
+                <div className="flex pl-10">
+                    <ul className="flex flex-col gap-4 w-full text-xl text-white">
+                        <button onClick={()=>setIsOpen(!isOpen)} className="flex justify-end w-full text-4xl p-5">x</button>
+                        <li>Home</li>
+                        <li>Menu</li>
+                        <li>Gallery</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+                </div>
+            </nav>
+    
     )
 }

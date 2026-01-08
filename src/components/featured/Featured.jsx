c2ec5d6b0551b70642652295d04e06983ead8dfb
Fenil Kapopara <fenilkapopara34@gmail.com>
2026-01-08 23:22:06 +0530

    image adjust

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -33,7 +33,7 @@ export const Featured = () => {
                     </li>
 
                 </ul>
-                <div className="relative min-h-screen mt-10 py-10">
+                <div className="relative h-160 mt-10 py-10">
                     <div className=" absolute translate-y-1/2 left-2 shadow-2xl bg-white p-2"><img src="./featured/image1.webp" alt="" className="w-45" /></div>
                     <div className="absolute right-5 bottom-15 shadow-2xl bg-white p-2"><img src="./featured/image2.webp" alt="" className="w-40" /></div>
                     <div className="absolute top-0 -z-10 shadow-2xl right-10 bg-white p-2"><img src="./featured/image3.webp" alt="" className="w-40" /></div>

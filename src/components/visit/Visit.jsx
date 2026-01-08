3857b0cf59e47d90f0a43c4e57e33f7a47bf29e3
Fenil Kapopara <fenilkapopara34@gmail.com>
2026-01-08 23:22:48 +0530

    clean map component

--- src/components/visit/Visit.jsx
+++ src/components/visit/Visit.jsx
@@ -1,33 +1,11 @@
+import { Map } from "./components/Map";
+
 export const Visit = () => {
   return (
-    <section className="my-10 p-4">
+    <section className="my-10 p-4 " id="visit">
         <p className="text-primary font-semibold">Visit Us</p>
         <h1 className="text-2xl font-bold font-heading">Made for Your<br />Moments.</h1>
-    <div className="relative py-4">
-      <div
-        style={{
-            position: "relative",
-            paddingBottom: "75%",
-          height: 0,
-          overflow: "hidden",
-        }}
-      >
-        <iframe
-          title="Bunglow Cafe Location"
-          src="https://maps.google.com/maps?q=the+bungalow+cafe+surat&output=embed"
-          style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-            width: "100%",
-            height: "100%",
-            border: 0,
-        }}
-        loading="lazy"
-          allowFullScreen
-        />
-      </div>
-    </div>
+        <Map/>
         </section>
   );
 };

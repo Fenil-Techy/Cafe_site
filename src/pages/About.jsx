ddd9a55c6ce52397620f50f773d3307ec8c8804c
Fenil Kapopara <fenilkapopara34@gmail.com>
2026-01-08 23:23:08 +0530

    create about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,15 @@
+import Carousel from "../components/about/Carousel"
+
 export const About = () => {
     return (
-        <div>
+        <div className="px-4 py-10">
             <div>
-                <h1 className="text-primary text-2xl">About Us</h1>
+                <Carousel/>
             </div>
-            <div>
+            <div className="mt-5">
+                <h1 className="text-primary font-subheading font-bold text-2xl">About Us</h1>
+            </div>
+            <div className="flex flex-col gap-4 mt-4 text-lg leading-8">
             <p>Located in the heart of Surat, Bunglow Cafe is more than just a café—it’s a refined escape from the everyday. Thoughtfully designed with elegance and comfort in mind, our space brings together exceptional coffee, fresh flavors, and an atmosphere that feels both calm and inspiring.</p>
 
             <p>Whether you’re meeting friends, focusing on work, enjoying meaningful conversations, or simply taking time for yourself, Bunglow Cafe adapts effortlessly to your moment. We believe great cafés don’t just serve coffee—they create experiences. Every detail, from our carefully crafted beverages to our welcoming ambience, is designed to make you feel relaxed, valued, and at home.</p>

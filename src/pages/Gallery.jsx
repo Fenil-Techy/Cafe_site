f8f133e3d7e61b348f28dbfc7fa1fa009ebdce0f
Fenil Kapopara <fenilkapopara34@gmail.com>
2026-01-08 23:23:26 +0530

    cfreate gallery page

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,7 +1,25 @@
 export const Gallery=()=>{
     return(
-        <div>
-            <h1>Gallery</h1>
+        <div className="p-4">
+            <div className="flex flex-col justify-center items-center gap-10">
+                <img src="/gallery/interior1.webp" alt="image of bungalow cafe" className="w-70 h-full rounded-4xl shadow-[2px_10px_30px_rgba(0,0,0,0.3)]" />
+                <img src="/gallery/food1.webp" alt="image of bungalow cafe" className="w-70 h-full rounded-4xl shadow-[2px_10px_30px_rgba(0,0,0,0.3)]"/>
+                <img src="/gallery/interior2.webp" alt="image of bungalow cafe" className="w-70 h-full rounded-4xl shadow-[2px_10px_30px_rgba(0,0,0,0.3)]"/>
+                <img src="/gallery/food2.webp" alt="image of bungalow cafe" className="w-70 h-full rounded-4xl shadow-[2px_10px_30px_rgba(0,0,0,0.3)]"/>
+                <img src="/gallery/interior3.webp" alt="image of bungalow cafe" className="w-80 h-full rounded-4xl shadow-[2px_10px_30px_rgba(0,0,0,0.3)]"/>
+                <img src="/gallery/food3.webp" alt="image of bungalow cafe" className="w-70 h-full rounded-4xl shadow-[2px_10px_30px_rgba(0,0,0,0.3)]"/>
+                <img src="/gallery/interior4.webp" alt="image of bungalow cafe" className="w-80 h-full rounded-4xl shadow-[2px_10px_30px_rgba(0,0,0,0.3)]"/>
+                <img src="/gallery/interior5.webp" alt="image of bungalow cafe" className="w-80 h-full rounded-4xl shadow-[2px_10px_30px_rgba(0,0,0,0.3)]"/>
+                <img src="/gallery/interior6.webp" alt="image of bungalow cafe" className="w-80 h-full rounded-4xl shadow-[2px_10px_30px_rgba(0,0,0,0.3)]"/>
+                <img src="/gallery/interior7.webp" alt="image of bungalow cafe" className="w-70 h-full rounded-4xl shadow-[2px_10px_30px_rgba(0,0,0,0.3)]"/>
+                <img src="/gallery/food4.webp" alt="image of bungalow cafe" className="w-70 h-full rounded-4xl shadow-[2px_10px_30px_rgba(0,0,0,0.3)]"/>
+                <img src="/gallery/interior8.webp" alt="image of bungalow cafe" className="w-70 h-full rounded-4xl shadow-[2px_10px_30px_rgba(0,0,0,0.3)]"/>
+                <img src="/gallery/food5.webp" alt="image of bungalow cafe" className="w-70 h-full rounded-4xl shadow-[2px_10px_30px_rgba(0,0,0,0.3)]"/>
+                <img src="/gallery/interior9.webp" alt="image of bungalow cafe" className="w-80 h-full rounded-4xl shadow-[2px_10px_30px_rgba(0,0,0,0.3)]"/>
+                <img src="/gallery/food6.webp" alt="image of bungalow cafe" className="w-70 h-full rounded-4xl shadow-[2px_10px_30px_rgba(0,0,0,0.3)]"/>
+                <img src="/gallery/interior10.webp" alt="image of bungalow cafe" className="w-70 h-full rounded-4xl shadow-[2px_10px_30px_rgba(0,0,0,0.3)]"/>
+                
+            </div>
         </div>
     )
 }
